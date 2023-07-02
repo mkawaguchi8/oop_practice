bbf7b8d3c24155f2fe5df226eafe507038942d42
mkawaguchi8 <minori22dmo@gmail.com>
2023-07-02 15:30:21 +0900

    self_introductionメソッドの追加

--- user_name.py
+++ user_name.py
@@ -1,18 +1,22 @@
-class UserName:
+class User:
     def __init__(self, name):
        if not (4 <= len(name) <= 20):
            raise ValueError(f"{name}はルール違反です")     
        self.name = name
 
+# screen_nameメソッドの作成
     def screen_name(self):
+        # upperは大文字に変換する
         return self.name.upper()
+
+# self_introctionメソッドの作成    
+    def self_introduction(self):
+        return f"私の名前は{self.name.upper()}です"
        
 
-
-# UserName クラスのインスタント化
-tanaka = UserName(name='tanaka')
+# Userクラスのインスタント化
+tanaka = User(name='tanaka')
 
 # 出力
 print(tanaka.screen_name())
-
-
+print(tanaka.self_introduction())
